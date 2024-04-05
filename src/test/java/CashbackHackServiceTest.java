2798de68bbf6f5d9a9f94d62c42567bd9a89a82c
mega test <mahp0@mail.ru>
2024-04-05 13:00:51 +0800

    junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,33 +1,37 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import ru.netology.CashbackHackService;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
-    public void ShouldFindMoneyWhenAmountLessThanBoundary() {
+    public void ShouldCalculateMoneyWhenAmountLessThanBoundary() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 666;
-        int expected = 334;
+        int amount = 550;
+        int expected = 450;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void ShouldFindMoneyWhenAmountEqualBoundary() {
+    public void ShouldCalculateMoneyWhenAmountEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void ShouldFindMoneyWhenAmountMoreThanBoundary() {
+    public void ShouldCalculateMoneyWhenAmountMoreBoundary() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1100;
-        int expected = 900;
+        int amount = 1001;
+        int expected = 999;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);
+
+        assertEquals(expected, actual);
     }
 }
-
